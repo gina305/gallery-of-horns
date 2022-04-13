77b81b1c2b26a8e7ff36fc9adb4da9d910529788
Gina Napier <napier.gina@gmail.com>
2022-04-13 00:04:54 -0500

    Completed Feature 2: Added event handler to capture clicks

--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -5,17 +5,30 @@ import './App.css' //Link CSS file
 
 
 class HornedBeast extends React.Component {
+constructor(props){
+  super(props);
+  this.state={
+    faves:0,
+  };
+}
 
-  
-  //Retrun JSX - which allows us to use javascript to render html
+favored = () => {
+this.setState({
+  faves: this.state.faves + 1
+});
+}
+  //Rerun JSX - which allows us to use javascript to render html
   render(title) {
 
     //Wrap JSX in empty tags (fragments)
     return <>
       <article id={this.props.id}>
         <h2>{title}</h2>
+        
         <img src={this.props.img} alt={this.props.title}></img>
-        <p >Title: {this.props.title}</p>
+            <p >Title: {this.props.title}</p>
+            <p>{this.state.faves}</p>
+            <p onClick={this.favored}>Favorite</p>
         <p>{this.props.description}</p>
         <p>Horns: {this.props.horns}</p>
         <p>Keyword(s): {this.props.keyword}</p>
